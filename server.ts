cda101c1749681b2096469593e11b7ed692620b3
wesleyyxie <wesleyxie05@gmail.com>
2025-03-07 21:56:23 -0500

    added custom port

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import { updateAnswerCron } from "./src/tasks/updateAnswerTask";
 
 const app: Express = require("./src");
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 updateAnswerCron();
 app.listen(port, () => {
