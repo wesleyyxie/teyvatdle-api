fa4880a0699d89f17e37a68544e5ab55a667c3a6
wesleyyxie <wesleyxie05@gmail.com>
2025-03-07 22:12:36 -0500

    testing Cronjob

--- src/tasks/updateAnswerTask.ts
+++ src/tasks/updateAnswerTask.ts
@@ -4,7 +4,7 @@ import { updateAnswers } from "../database/updateHistories";
 export function updateAnswerCron() {
   console.log("Setting up Cron Job");
   cron.schedule(
-    "12 3 * * *",
+    "14 3 * * *",
     async () => {
       await updateAnswers();
     },
