7b9e69401b3ecb44657ac71ad377864e9f8e0061
wesleyyxie <wesleyxie05@gmail.com>
2025-03-08 17:05:41 -0500

    Adjusting cronjob for testing

--- src/tasks/updateAnswerTask.ts
+++ src/tasks/updateAnswerTask.ts
@@ -4,7 +4,7 @@ import { updateAnswers } from "../database/updateHistories";
 export function updateAnswerCron() {
   console.log("Setting up Cron Job");
   cron.schedule(
-    "8 10 * * *",
+    "12 20 * * *",
     async () => {
       await updateAnswers();
     },
